fe54d24934359f6c1058d72c308b2e182667ed8f
Teddy Hartanto <hartantoteddy@yahoo.com>
2017-11-17 18:22:49 +0800

    Better naming

--- pyramid.py
+++ pyramid.py
@@ -5,17 +5,17 @@ import numpy as np
 WIN_SIZE = 3
 
 
-def downsize_frame(source):
+def downsize_frame(frame):
     is_color = False
-    # print(source.shape)
+
     try:
-        rows, cols, _ = source.shape  # color
-        half_rows, half_cols = rows / 2, cols / 2
+        rows, cols, _ = frame.shape  # color
+        half_rows, half_cols = rows // 2, cols // 2
         downsized_frame = np.zeros([half_rows, half_cols, 3])
         is_color = True
     except ValueError:
-        rows, cols = source.shape  # greyscale
-        half_rows, half_cols = rows / 2, cols / 2
+        rows, cols = frame.shape  # greyscale
+        half_rows, half_cols = rows // 2, cols // 2
         downsized_frame = np.zeros([half_rows, half_cols])
 
     gkern1d = signal.gaussian(WIN_SIZE, std=1).reshape(WIN_SIZE, 1)
@@ -28,9 +28,9 @@ def downsize_frame(source):
     else:
         gkern3d = gkern2d
 
-    g_source = signal.convolve(source, gkern3d, mode='same')
+    blurred_frame = signal.convolve(frame, gkern3d, mode='same')
     for row in range(0, half_rows):
         for col in range(0, half_cols):
-            downsized_frame[row, col] = (g_source[row * 2, col * 2])
+            downsized_frame[row, col] = (blurred_frame[row * 2, col * 2])
 
     return np.uint8(downsized_frame)
